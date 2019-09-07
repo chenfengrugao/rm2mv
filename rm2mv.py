da677b52dac39f4c869b96a1488b682de8696dd6
billc <billc@localhost.localdomain>
2019-09-07 22:39:29 +0800

    update

--- rm2mv.py
+++ rm2mv.py
@@ -5,16 +5,18 @@
 #   1.0 Initial
 #   1.1 fix bugs
 #   1.2 fix bugs
-#   1.3 21/5/2013 add help options
-#   1.4 23/5/2013 move the deleted files to ~/.Trash/datatime folder 
-#                   to avoid the files with the same name
-#   1.5 29/5/2013 do not move when $MyFileList is empty
-#   1.6 2/9/2013  change 'unix rm' to 'linux rm', and add a bug notes
-#   1.7 16/9/2013 change default to silence mode
-#                 move to trash only when the files exist.
-#   1.8 9/8/2019  implement with python
-#   1.9 5/9/2019  fix bug of cleaning ~/.Trash
-#                 add feature: calc the size of ~/.Trash with linux cmd `du -sh ~/.Trash`
+#   1.3 21/5/2013  add help options
+#   1.4 23/5/2013  move the deleted files to ~/.Trash/datatime folder 
+#                    to avoid the files with the same name
+#   1.5 29/5/2013  do not move when $MyFileList is empty
+#   1.6 2/9/2013   change 'unix rm' to 'linux rm', and add a bug notes
+#   1.7 16/9/2013  change default to silence mode
+#                  move to trash only when the files exist.
+#   1.8 9/8/2019   implement with python
+#   1.9 5/9/2019   fix bug of cleaning ~/.Trash
+#                  add feature: calc the size of ~/.Trash with linux cmd `du -sh ~/.Trash`
+#   1.10 7/9/2019  replace os.system('mv xx') with shutil.move to handle file with special chars.
+#                  check ~/.Trash if exists first, when --clean or --status
 #
 
 import os
@@ -66,11 +68,15 @@ for arg in sys.argv[1:]:
 
     # clean .Trash before 1 week
     elif arg == '--status':
+        if not os.path.exists('/home/{}/.Trash'.format(user)):
+            sys.exit()
         os.system('du -sh /home/{}/.Trash'.format(user))
         sys.exit()
         
     # clean .Trash before 1 week
     elif arg == '--clean':
+        if not os.path.exists('/home/{}/.Trash'.format(user)):
+            sys.exit()
         dirs = os.listdir('/home/{}/.Trash'.format(user))
         for dt in dirs:
             if((datetime.datetime.now() - datetime.datetime(int(dt[0:4]), int(dt[4:6]), int(dt[6:8]))).days > 7):
@@ -99,5 +105,7 @@ if len(MyFileList) != 0:
     target = '/home/{}/.Trash/{}'.format(user, date_time_dir)
     if not os.path.exists(target):
         os.makedirs(target)
-    os.system('mv {} {}/'.format(' '.join(MyFileList), target))
+    #print('mv {} {}/'.format(' '.join(MyFileList), target))
+    for f in MyFileList:
+        shutil.move(f, target)
 
